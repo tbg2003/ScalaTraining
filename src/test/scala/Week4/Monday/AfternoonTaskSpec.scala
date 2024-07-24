0b76a8b273d11f1b5485c7222aa377557f1745a6
Tom Burt-Gray <tom.burt.gray@Toms-Laptop.local>
2024-07-24 09:47:33 +0100

    Afternoon Task

--- src/test/scala/Week4/Monday/AfternoonTaskSpec.scala
+++ src/test/scala/Week4/Monday/AfternoonTaskSpec.scala
@@ -1,5 +1,33 @@
 package Week4.Monday
 
-class AfternoonTaskSpec {
+import Week4.Monday.AfternoonTask._
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
 
+class AfternoonTaskSpec extends AnyWordSpec with Matchers{
+
+  "fibonacciCalc method" should {
+    "return a Left" when {
+      "The value of n is negative" in {
+        fibonacciCalc(-1) shouldEqual Left("Error: input must be positive, -1 < 0")
+      }
+    }
+
+    "return a Right" when {
+
+      "The value of n is greater than 2" in {
+        fibonacciCalc(5) shouldEqual Right(Seq(1, 1, 2, 3, 5))
+      }
+      // Edge Cases
+      "The value of n is zero" in {
+        fibonacciCalc(0) shouldEqual Right(Seq())
+      }
+      "The value of n is 1" in {
+        fibonacciCalc(1) shouldEqual Right(Seq(1))
+      }
+      "The value of n is 2" in {
+        fibonacciCalc(2) shouldEqual Right(Seq(1, 1))
+      }
+    }
+  }
 }
